69db6103611b9b151d3f6882c095c44ee74d9a0f
celestine <okorocelestine@gmail.com>
2017-04-21 18:58:11 +0100

    add twitter, git and facebook auth

--- public/js/login.js
+++ public/js/login.js
@@ -69,6 +69,201 @@ $(document).ready(function(){
     }); 
   });
   
+   $("#facebooksignin").click(function() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth()
+   .signInWithPopup(provider)
+   .then(function(result) {
+      var token = result.credential.accessToken;
+      var curuser = result.user;
+      //console.log(result.user);
+      console.log(curuser.displayName + '-' + curuser.email + '- ' + curuser.photoURL + '' +curuser.uid );
+      //check if user exist in db
+      let Userref = firebase.database().ref('ist/user');
+      Userref.orderByChild("uid").equalTo(curuser.uid).once("value", function(snapshot) {
+        var value = snapshot.val();
+        var keys = Object.keys(value);
+        var userinfo =value[keys[0]];
+        if (userinfo){
+          $.post("/setsession", {uid: curuser.uid}, function(responseData){ 
+            console.log(responseData, 'responseData'); 
+            if(responseData) {
+              if(userinfo.role) {
+               window.location.href = '/openissue';
+              } else {
+                window.location.href = '/issuelog';
+              }
+            }
+          });
+        } else {
+          // create new user
+          var  newuser = {
+            "name" : curuser.displayName,
+            "departments" : "",
+            "uid" : curuser.uid ,
+            "email" : curuser.email,
+            "phone" :"",
+            "pic" : curuser.photoURL
+          };
+          //console.log(localStorage.uid);
+          userRef.push(newuser).then(function(user) {
+            //set session 
+             $.post("/setsession", {uid: curuser.uid}, function(responseData){ 
+                console.log(responseData, 'responseData'); 
+                if(responseData) {
+                  window.location.href = '/profile';
+                }
+              });
+            // direct user to profile
+            
+          }). catch(function(error) {
+            showresult("Error occures, please try again");
+            //$("#result").text = "Error occures, please try again";
+            //console.error('Sign Out Error', error);
+            
+          });
+        }
+      });
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+     // res.redirect('/signup');
+     // console.log(error.code)
+       //console.log(error.message)
+      showresult("Error occures, please try again");
+    }); 
+  });
+
+ $("#twittersigin").click(function() {
+  var provider = new firebase.auth.TwitterAuthProvider();
+  firebase.auth()
+   .signInWithPopup(provider)
+   .then(function(result) {
+      var token = result.credential.accessToken;
+      var curuser = result.user;
+      //console.log(result.user);
+      console.log(curuser.displayName + '-' + curuser.email + '- ' + curuser.photoURL + '' +curuser.uid );
+      //check if user exist in db
+      let Userref = firebase.database().ref('ist/user');
+      Userref.orderByChild("uid").equalTo(curuser.uid).once("value", function(snapshot) {
+        var value = snapshot.val();
+        var keys = Object.keys(value);
+        var userinfo =value[keys[0]];
+        if (userinfo){
+          $.post("/setsession", {uid: curuser.uid}, function(responseData){ 
+            console.log(responseData, 'responseData'); 
+            if(responseData) {
+              if(userinfo.role) {
+               window.location.href = '/openissue';
+              } else {
+                window.location.href = '/issuelog';
+              }
+            }
+          });
+        } else {
+          // create new user
+          var  newuser = {
+            "name" : curuser.displayName,
+            "departments" : "",
+            "uid" : curuser.uid ,
+            "email" : curuser.email,
+            "phone" :"",
+            "pic" : curuser.photoURL
+          };
+          //console.log(localStorage.uid);
+          userRef.push(newuser).then(function(user) {
+            //set session 
+             $.post("/setsession", {uid: curuser.uid}, function(responseData){ 
+                console.log(responseData, 'responseData'); 
+                if(responseData) {
+                  window.location.href = '/profile';
+                }
+              });
+            // direct user to profile
+            
+          }). catch(function(error) {
+            showresult("Error occures, please try again");
+            //$("#result").text = "Error occures, please try again";
+            //console.error('Sign Out Error', error);
+            
+          });
+        }
+      });
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+     // res.redirect('/signup');
+     // console.log(error.code)
+       //console.log(error.message)
+      showresult("Error occures, please try again");
+    }); 
+  });
+
+ $("#gitsignin").click(function() {
+  var provider = new firebase.auth.GithubAuthProvider();
+  firebase.auth()
+   .signInWithPopup(provider)
+   .then(function(result) {
+      var token = result.credential.accessToken;
+      var curuser = result.user;
+      //console.log(result.user);
+      console.log(curuser.displayName + '-' + curuser.email + '- ' + curuser.photoURL + '' +curuser.uid );
+      //check if user exist in db
+      let Userref = firebase.database().ref('ist/user');
+      Userref.orderByChild("uid").equalTo(curuser.uid).once("value", function(snapshot) {
+        var value = snapshot.val();
+        var keys = Object.keys(value);
+        var userinfo =value[keys[0]];
+        if (userinfo){
+          $.post("/setsession", {uid: curuser.uid}, function(responseData){ 
+            console.log(responseData, 'responseData'); 
+            if(responseData) {
+              if(userinfo.role) {
+               window.location.href = '/openissue';
+              } else {
+                window.location.href = '/issuelog';
+              }
+            }
+          });
+        } else {
+          // create new user
+          var  newuser = {
+            "name" : curuser.displayName,
+            "departments" : "",
+            "uid" : curuser.uid ,
+            "email" : curuser.email,
+            "phone" :"",
+            "pic" : curuser.photoURL
+          };
+          //console.log(localStorage.uid);
+          userRef.push(newuser).then(function(user) {
+            //set session 
+             $.post("/setsession", {uid: curuser.uid}, function(responseData){ 
+                console.log(responseData, 'responseData'); 
+                if(responseData) {
+                  window.location.href = '/profile';
+                }
+              });
+            // direct user to profile
+            
+          }). catch(function(error) {
+            showresult("Error occures, please try again");
+            //$("#result").text = "Error occures, please try again";
+            //console.error('Sign Out Error', error);
+            
+          });
+        }
+      });
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+     // res.redirect('/signup');
+     // console.log(error.code)
+       //console.log(error.message)
+      showresult("Error occures, please try again");
+    }); 
+  });
+  
   $("#signin").submit(function(event) {
     /* stop form from submitting normally */
     event.preventDefault();
